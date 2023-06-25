f99868b40b6c9342c020b1ef659b8d53cd72b28e
westerntc <veretax@gmail.com>
2023-06-25 16:08:33 -0400

    Getting first repo setup (Notice differences between master/main in local git and Github)
    We got specflow on a basic calculator app... and now we need to start working on this

---
+++ BattletechModInfoLibrary.SpecFlow/Features/FileManager.feature
@@ -0,0 +1,11 @@
+﻿Feature: FileManager
+
+A file manager for getting file information and managing their attributes
+
+
+@FileManager
+Scenario: Empty File still has file info
+	Given A path to a folder
+	And the file name is
+	When the combined path is checked
+	Then the file info name should exist
---
+++ BattletechModInfoLibrary.SpecFlow/StepDefinitions/FileManagerStepDefinitions.cs
@@ -0,0 +1,46 @@
+﻿namespace BattletechModInfo.Library.SpecFlow.StepDefinitions;
+
+public class FileManagerStepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [Given("A path to a folder (.*)")]
+        public void GivenAPathToAFolder(string path)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method.
+
+            throw new PendingStepException();
+        }
+
+        [Given("the file name is (.*)")]
+        public void GivenTheFileNameIs(string filename)
+        {
+            //TODO: implement arrange (precondition) logic
+
+            throw new PendingStepException();
+        }
+
+        [When("combined path is checked")]
+        public void WhenTheCombinedPathIsChecked()
+        {
+            //TODO: implement act (action) logic
+
+            throw new PendingStepException();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            throw new PendingStepException();
+        }
+    }
+}
